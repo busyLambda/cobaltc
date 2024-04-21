87215fc3804bce01ce2de539382b0267c7882f81
Susa Milán <mailtosnek@protonmail.com>
2024-04-21 17:00:32 +0200

    Complex error handling + testing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod parser;
 
 fn main() {
-    let input = "func filter(a [int]) [int]";
+    let input = "func add(a int, b int)";
     let mut lexer = parser::lexer::Lexer::new(input);
     let tokens = lexer.lex();
-    let mut parser = parser::Parser::new(&tokens);
-    let func = parser.func().unwrap();
-    println!("{:?}", func)
+    let mut parser = parser::parser::Parser::new(&tokens);
+    let func = parser.func();
+    println!("{:?}", func);
+    parser.errors().into_iter().for_each(|e| println!("Error: {:?}", e));
 }
---
+++ src/parser/error.rs
@@ -0,0 +1,84 @@
+use std::ops::Range;
+
+#[derive(Debug)]
+pub struct PropagatorError {
+    message: String,
+    location: Location,
+}
+
+impl PropagatorError {
+    pub fn new(message: &str, location: Location) -> Self {
+        Self {
+            message: message.to_string(),
+            location,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct StructureError {
+    message: String,
+    propagator_location: Location,
+    location: Location,
+}
+
+impl StructureError {
+    pub fn from_propagator(value: PropagatorError, location: Location) -> Self {
+        Self {
+            message: value.message,
+            propagator_location: value.location,
+            location,
+        }
+    }
+
+    pub fn new(message: &str, propagator_location: Location, location: Location) -> Self {
+        Self {
+            message: message.to_string(),
+            propagator_location,
+            location,
+        }
+    }
+}
+
+pub type PropagatorResult<T> = Result<T, (T, PropagatorError)>;
+#[derive(Debug)]
+pub struct ParseError {
+    message: String,
+    location: ErrorLocation,
+}
+
+impl ParseError {
+    pub fn new(message: String, location: ErrorLocation) -> ParseError {
+        ParseError { message, location }
+    }
+}
+
+#[derive(Debug)]
+pub struct ErrorLocation {
+    structure: Location,
+    // Structures don't return errors while propagator parsers do,
+    // These errors travel up the chain until the structure reports it.
+    propagator: Location,
+}
+
+impl ErrorLocation {
+    pub fn new() -> ErrorLocation {
+        ErrorLocation {
+            structure: Location { span: 0..0 },
+            propagator: Location { span: 0..0 },
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Location {
+    span: Range<usize>,
+}
+
+impl Location {
+    pub fn new(span: Range<usize>) -> Self {
+        Self { span }
+    }
+}
+
+pub type ParseResult<T> = Result<T, ParseError>;
